3b518339ed5b0801ffa2f984d578fe04ba791f3b
jjong0225 <jjong0225@naver.com>
2025-06-17 12:21:16 +0000

    Deploying to gh-pages from @ jjong0225/jjong0225.github.io@b469f89c52fda7cd140fb1a80ef712af0bdf3c0e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -532,7 +532,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%6A%6F%6E%67%68%6F%6F%6E%30%32%32%35@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
