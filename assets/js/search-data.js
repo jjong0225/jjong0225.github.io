a44230a45aca230de593c94f0ddd048ef77773e0
jjong0225 <jjong0225@naver.com>
2025-08-12 09:06:16 +0000

    Deploying to gh-pages from @ jjong0225/jjong0225.github.io@acefae89815047930b0681b8c6999a98f822266b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -506,11 +506,11 @@ ninja.data = [{
           window.open("https://github.com/jjong0225", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/종훈-안-336851312", "_blank");
         },
       },{
         id: 'social-rss',
@@ -518,20 +518,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
